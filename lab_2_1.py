7734a648d0bd966b6a46d6fc6df69e1d0b976914
grimaximov <grimaximov@gmail.com>
2023-04-06 06:29:29 +0300

    lab_2_1

---
+++ lab_2_1.py
@@ -0,0 +1,8 @@
+list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
+list_sum = sum(set(list_))
+list_len = len(set(list_))
+list_med = round(list_sum/list_len,5)
+print(list_sum)
+print(list_len)
+print(list_med)
+# TODO найти сумму, количество и среднее арифметическое уникальных чисел
